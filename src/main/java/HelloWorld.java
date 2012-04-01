10166226e441af14ea3276469975af936e09a209
Nicolas Hurion <nicolas@hurion.eu>
2012-04-01 08:42:26 +0200

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import com.bsb.common.vaadin.embed.EmbedVaadinServer;
+import com.bsb.common.vaadin.embed.support.EmbedVaadin;
+import com.vaadin.ui.Label;
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        final EmbedVaadinServer server = EmbedVaadin.forComponent(new Label("hello world")).withHttpPort(8080).start();
+    }
+}
