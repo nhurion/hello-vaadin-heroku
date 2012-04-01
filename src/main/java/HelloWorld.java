84401ba406f46786b52950d6beaf7d4e4d5faef8
Nicolas Hurion <nicolas@hurion.eu>
2012-04-01 19:34:52 +0200

    improve README, use constants.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,15 +1,17 @@
-import com.bsb.common.vaadin.embed.EmbedVaadinServer;
 import com.bsb.common.vaadin.embed.support.EmbedVaadin;
 import com.vaadin.ui.Label;
 
 
 public class HelloWorld {
 
+    public static final String PORT = "PORT";
+    public static final String HELLO_WORLD = "Hello World!";
+
     public static void main(String[] args) {
-        int httpPort = Integer.parseInt(System.getenv("PORT"));
+        int httpPort = Integer.parseInt(System.getenv(PORT));
         EmbedVaadin.forComponent(
-                        new Label("hello world"))
-                    .withHttpPort(httpPort)
-                    .start();
+                new Label(HELLO_WORLD))
+                .withHttpPort(httpPort)
+                .start();
     }
 }
