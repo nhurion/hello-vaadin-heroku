4dc075c78f1de904554ab2082d52dde90a498a2f
Nicolas Hurion <nicolas@hurion.eu>
2012-04-09 16:07:36 +0200

    add logback, vaadin maven repository and target as ignore

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,13 +5,23 @@ import com.vaadin.ui.Label;
 public class HelloWorld {
 
     public static final String PORT = "PORT";
+    public static final int DEFAULT_PORT = 8080;
     public static final String HELLO_WORLD = "Hello World!";
 
     public static void main(String[] args) {
-        int httpPort = Integer.parseInt(System.getenv(PORT));
+        int httpPort = getPort();
         EmbedVaadin.forComponent(
                 new Label(HELLO_WORLD))
                 .withHttpPort(httpPort)
+
                 .start();
+    }
+
+    private static int getPort() {
+        final String envPort = System.getenv(PORT);
+        if (envPort == null) {
+            return DEFAULT_PORT;
+        }
+        return Integer.parseInt(envPort);
     }
 }
