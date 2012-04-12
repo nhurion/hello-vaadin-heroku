17c3b26ed24f35d65d59b4d41b280dc48861e454
Nicolas Hurion <nicolas@hurion.eu>
2012-04-12 22:50:14 +0200

    Interactive Hello

--- src/main/java/eu/hurion/hello/vaadin/HelloHerokuApplication.java
+++ src/main/java/eu/hurion/hello/vaadin/HelloHerokuApplication.java
@@ -1,20 +1,46 @@
 package eu.hurion.hello.vaadin;
 
 import com.vaadin.Application;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
+import com.vaadin.ui.*;
 
 public class HelloHerokuApplication extends Application {
 
-    public static final String HELLO_WORLD = "Hello from Heroku!";
+    public static final String HELLO_WORLD = "Hello from Heroku, ";
+    public static final String BUTTON_CAPTION = "Click me";
+    public static final String NAME_LABEL = "What is your name?";
 
     @Override
     public void init() {
         final Window window = new Window();
         setMainWindow(window);
-        final VerticalLayout mainLayout = new VerticalLayout();
-        window.addComponent(mainLayout);
-        mainLayout.addComponent(new Label(HELLO_WORLD));
+        window.setContent(buildContent());
+
+    }
+
+    private ComponentContainer buildContent() {
+        final FormLayout formLayout = new FormLayout();
+        formLayout.setSpacing(true);
+        formLayout.setSizeUndefined();
+
+        final TextField nameInput = new TextField();
+        nameInput.setCaption(NAME_LABEL);
+        formLayout.addComponent(nameInput);
+
+        final Button showButton = new Button(BUTTON_CAPTION, new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                getMainWindow().showNotification(
+                        HELLO_WORLD + nameInput.getValue() + " !",
+                        ""
+                );
+
+            }
+        });
+        formLayout.addComponent(showButton);
+        final VerticalLayout vl = new VerticalLayout();
+        vl.addComponent(formLayout);
+        vl.setComponentAlignment(formLayout, Alignment.MIDDLE_CENTER);
+        vl.setSizeFull();
+        return vl;
     }
 }
