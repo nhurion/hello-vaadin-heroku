142de296ed6e92334cdf3b1f58df9dee403719e6
Nicolas Hurion <nicolas@hurion.eu>
2012-10-13 22:28:16 +0200

    update to vaadin-for-heroku 0.3

--- src/main/java/eu/hurion/hello/vaadin/Launcher.java
+++ src/main/java/eu/hurion/hello/vaadin/Launcher.java
@@ -2,11 +2,12 @@ package eu.hurion.hello.vaadin;
 
 import eu.hurion.hello.vaadin.application.HelloHerokuApplication;
 
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.herokuServer;
 
 public class Launcher {
 
     public static void main(final String[] args) {
-            herokuServer(HelloHerokuApplication.class).start();
+            herokuServer(forApplication(HelloHerokuApplication.class)).start();
     }
 }
--- src/test/java/eu/hurion/hello/vaadin/DevLauncher.java
+++ src/test/java/eu/hurion/hello/vaadin/DevLauncher.java
@@ -2,10 +2,11 @@ package eu.hurion.hello.vaadin;
 
 import eu.hurion.hello.vaadin.application.HelloHerokuApplication;
 
+import static eu.hurion.vaadin.heroku.VaadinForHeroku.forApplication;
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.localServer;
 
 public class DevLauncher {
     public static void main(final String[] args) {
-        localServer(HelloHerokuApplication.class).start();
+        localServer(forApplication(HelloHerokuApplication.class)).start();
     }
 }
