3a52ef038199a9df33871931150ec094a3045823
Nicolas Hurion <nicolas@hurion.eu>
2013-08-11 22:39:52 +0200

    migrate to vaadin 7.1.2

--- src/main/java/eu/hurion/hello/vaadin/application/HelloHerokuApplication.java
+++ src/main/java/eu/hurion/hello/vaadin/application/HelloHerokuApplication.java
@@ -1,23 +1,22 @@
 package eu.hurion.hello.vaadin.application;
 
-import com.vaadin.Application;
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HelloHerokuApplication extends Application {
+public class HelloHerokuApplication extends UI {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloHerokuApplication.class);
 
     public static final String HELLO_WORLD = "Hello from Heroku, ";
     public static final String BUTTON_CAPTION = "Click me";
     public static final String NAME_LABEL = "What is your name?";
+    public static final String NAME = "name";
 
     @Override
-    public void init() {
-        final Window window = new Window();
-        setMainWindow(window);
-        window.setContent(buildContent());
+    public void init(final VaadinRequest vaadinRequest) {
+        setContent(buildContent());
     }
 
     private ComponentContainer buildContent() {
@@ -27,14 +26,19 @@ public class HelloHerokuApplication extends Application {
 
         final TextField nameInput = new TextField();
         nameInput.setCaption(NAME_LABEL);
+        final String storedName = (String) getUI().getSession().getAttribute(NAME);
+        if (storedName != null){
+            nameInput.setValue(storedName);
+        }
         formLayout.addComponent(nameInput);
 
         final Button showButton = new Button(BUTTON_CAPTION, new Button.ClickListener() {
             @Override
             public void buttonClick(final Button.ClickEvent clickEvent) {
+                getUI().getSession().setAttribute(NAME, nameInput.getValue());
                 final String greeting = HELLO_WORLD + nameInput.getValue() + " !";
                 LOG.debug(greeting);
-                getMainWindow().showNotification(greeting, "");
+                Notification.show(greeting);
             }
         });
         formLayout.addComponent(showButton);
@@ -44,4 +48,5 @@ public class HelloHerokuApplication extends Application {
         vl.setSizeFull();
         return vl;
     }
+
 }
